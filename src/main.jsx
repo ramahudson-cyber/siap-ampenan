dc556394431f2e76d52d21d873487fbbad27239c
ramahudson-cyber <ramahudson@gmail.com>
2026-06-18 23:13:39 +0800

    Fix: update import paths to CapitalCase (match renamed files)

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "./context/themecontext";
+import { ThemeProvider } from "./context/ThemeContext";
 
 import "./index.css";
 
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "../pages/auth/LoginPage";
-import DashboardPage from "../pages/admin/DashboardPage";
+import DashboardPage from ".../pages/admin/DashboardPage";
 import EmployeesPage from "../pages/admin/EmployeesPage";
 import AttendancePage from "../pages/attendance/AttendancePage";
 import AttendanceHistoryPage from "../pages/admin/AttendanceHistoryPage";
